22772e3c14c4309f4412aba8f07ed19fabe85391
Zhenhong Zhang <t-zhezhang@microsoft.com>
2021-06-20 20:20:32 +0800

    New a project Large-scale-data-visualization-platform which is based on Spring framwork.

---
+++ Large-scale-data-visualization-platform/src/main/java/polyu/dsa/largescaledatavisualizationplatform/LargeScaleDataVisualizationPlatformApplication.java
@@ -0,0 +1,13 @@
+package polyu.dsa.largescaledatavisualizationplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LargeScaleDataVisualizationPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LargeScaleDataVisualizationPlatformApplication.class, args);
+    }
+
+}
---
+++ Large-scale-data-visualization-platform/src/test/java/polyu/dsa/largescaledatavisualizationplatform/LargeScaleDataVisualizationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package polyu.dsa.largescaledatavisualizationplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LargeScaleDataVisualizationPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
